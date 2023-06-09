8ffef992aa8f192226e35e4cc7f4339ef51203e8
@NicBeans <nicholasbosman007@gmail.com>
2023-06-09 17:37:38 +0200

    CRUD OPERATION SUCCESS DB connection instantiated, all is well. DETAILS ARE PUBLIC< NEED TO FIX .ENV FILES

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,16 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
+import 'package:mysql1/mysql1.dart';
 
 import 'package:flutter/widgets.dart';
 
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 Future<void> main() async {
-  await dotenv.load(); // Load the environment variables
-
+  //await dotenv.load(); // Load the environment variables
   runApp(MyApp());
 }
 
-
-class EmployeeData {
-  final int empID;
-  final String fname;
-  final String lname;
-  final bool isManager;
-  final String department;
-  final String email;
-  final String password;
-
-  const EmployeeData({
-    required this.empID,
-    required this.fname,
-    required this.lname,
-    required this.isManager,
-    required this.department,
-    required this.email,
-    required this.password
-  });
-}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -62,6 +42,110 @@ class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
   bool _showCounter = false;
 
+  Future<void> connect(BuildContext ctx) async {
+    print('about to exec');
+    debugPrint("Connecting...");
+    try {
+      print('execing');
+      showDialog(
+        context: context,
+        builder: (context) {
+          return const AlertDialog(
+            title: Text("LOADING"),
+            content: CircularProgressIndicator(),
+          );
+        },
+      );
+      final conn = await MySqlConnection.connect(ConnectionSettings(
+          host: 'db-mysql-fra1-itmda-do-user-14181038-0.b.db.ondigitalocean.com',
+          port: 25060,
+          user: 'doadmin',
+          db: 'g2gsystemdata',
+          password: 'AVNS_x9MsCEPSYvujay1CtiH'));
+      debugPrint("Connected!");
+
+      var result = await conn.query(
+          'INSERT INTO EMPLOYEE (empID, f_name, l_name, isManager, department, email, password) values (1041, \'Emma1234\', \'Walker1\', 1, \'Marketing1\', \'em1ma.walker@example.com\', \'passw1ord10\')'
+      );
+      print('Inserted row id=${result.insertId}');
+    } catch (e) {
+      debugPrint(e.toString());
+      print(e.toString());
+    } finally {
+      Navigator.pop(context);
+    }
+  }
+
+  Future<void> delete(BuildContext ctx) async {
+    print('about to exec');
+    debugPrint("Connecting...");
+    try {
+      print('execing');
+      showDialog(
+        context: context,
+        builder: (context) {
+          return const AlertDialog(
+            title: Text("LOADING"),
+            content: CircularProgressIndicator(),
+          );
+        },
+      );
+      final conn = await MySqlConnection.connect(ConnectionSettings(
+          host: 'db-mysql-fra1-itmda-do-user-14181038-0.b.db.ondigitalocean.com',
+          port: 25060,
+          user: 'doadmin',
+          db: 'g2gsystemdata',
+          password: 'AVNS_x9MsCEPSYvujay1CtiH'));
+      debugPrint("Connected!");
+
+      var result = await conn.query(
+          'DELETE FROM EMPLOYEE WHERE empID = 1041'
+      );
+      print('Deleted row id=${result.insertId}');
+    } catch (e) {
+      debugPrint(e.toString());
+      print(e.toString());
+    } finally {
+      Navigator.pop(context);
+    }
+  }
+
+  Future<void> read(BuildContext ctx) async {
+    print('about to exec');
+    debugPrint("Connecting...");
+    try {
+      print('execing');
+      showDialog(
+        context: context,
+        builder: (context) {
+          return const AlertDialog(
+            title: Text("LOADING"),
+            content: CircularProgressIndicator(),
+          );
+        },
+      );
+      final conn = await MySqlConnection.connect(ConnectionSettings(
+          host: 'db-mysql-fra1-itmda-do-user-14181038-0.b.db.ondigitalocean.com',
+          port: 25060,
+          user: 'doadmin',
+          db: 'g2gsystemdata',
+          password: 'AVNS_x9MsCEPSYvujay1CtiH'));
+      debugPrint("Connected!");
+
+      var result = await conn.query(
+          'SELECT FROM EMPLOYEE WHERE empID = 1041'
+      );
+      print('Selected row id=${result.insertId}');
+    } catch (e) {
+      debugPrint(e.toString());
+      print(e.toString());
+    } finally {
+      Navigator.pop(context);
+    }
+  }
+
+
+
   void _incrementCounter() {
     setState(() {
       _counter++;
@@ -81,32 +165,46 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            if (_showCounter) ...[
-              const Text(
-                'You have pushed the button this many times:',
-              ),
-              Text(
-                '$_counter',
-                style: Theme.of(context).textTheme.titleLarge,
-              ),
-            ],
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: _toggleCounterVisibility,
-              child: Text(_showCounter ? 'Hide Counter' : 'Show Counter'),
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      // body: Center(
+      //   child: Column(
+      //     mainAxisAlignment: MainAxisAlignment.center,
+      //     children: <Widget>[
+      //       if (_showCounter) ...[
+      //         const Text(
+      //           'You have pushed the button this many times:',
+      //         ),
+      //         Text(
+      //           '$_counter',
+      //           style: Theme.of(context).textTheme.titleLarge,
+      //         ),
+      //       ],
+      //       const SizedBox(height: 20),
+      //       ElevatedButton(
+      //         onPressed: _toggleCounterVisibility,
+      //         child: Text(_showCounter ? 'Hide Counter' : 'Show Counter'),
+      //       ),
+      //
+      //     ],
+      //   ),
+      // ),
+        body: Center(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                ElevatedButton(
+                  onPressed: () => connect(context),
+                  child: const Text("Connect")),
+                ElevatedButton(
+                  onPressed: () => delete(context),
+                  child: const Text("Delete")),
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: _incrementCounter,
+      //   tooltip: 'Increment',
+      //   child: const Icon(Icons.add),
+      // ),
+    ])
+        )
     );
   }
 }
